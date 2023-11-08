a6f47d1ca7cfbd9f72be4889504d255bdf64e297
Khoa Le <106197609+ArritakiItto@users.noreply.github.com>
2023-11-07 19:18:16 -0800

    Delete backend directory

--- backend/app.test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest');
-const app = require('./app'); // Path to app.js
-
-describe('App Main Entry Point', () => {
-  it('should respond with a 200 status for the root route', async () => {
-    const res = await request(app).get('/');
-    expect(res.statusCode).toEqual(200);
-  });
-});
